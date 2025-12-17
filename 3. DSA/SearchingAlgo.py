1f4c5b91fb03c7a9a8018fd017247c288ec356b4
Mayank Singh <mayank.techx@gmail.com>
2025-12-18 00:23:12 +0530

    feat: add binary search algorithm implementation with example usage

--- 3. DSA/SearchingAlgo.py
+++ 3. DSA/SearchingAlgo.py
@@ -1,3 +1,21 @@
 import sys
 sys.stdin = open('/Users/mayank/Development/Coding Env/4. PYTHON/input.txt', 'r')
 sys.stdout = open('/Users/mayank/Development/Coding Env/4. PYTHON/output.txt', 'w')
+
+# Binary Search
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+arr = [1,3,5,7,9,10]
+target = 3
+print(binary_search(arr, target))
